93bfc4bec2ad86ff22da9976c22767091b3071be
Asim <asim.h@simply-now.co.uk>
2025-07-31 17:12:26 +0100

    added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'app_icon_512.png', // app icon
+        'bg_pattern_blue.jpg', // background image
+        'rawdah_mosque_logo.png' // logo
+      ],
+      manifest: {
+        name: 'Rawdah Mosque',
+        short_name: 'Rawdah',
+        description: 'Live daily prayer times and announcements for Rawdah Mosque.',
+        theme_color: '#56853c',
+        background_color: '#163c4c',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: '/app_icon_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/app_icon_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/app_icon_144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
